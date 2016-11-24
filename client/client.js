6d4b47a07dd956ab1d0e873e52f6a4e0e8fbd8d0
Brynjar Daði Magnússon <FS432653@fss.is>
2016-11-24 09:17:56 +0000

    Bætti við api ofl

--- client/client.js
+++ client/client.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from '../components/App'
-
-
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 render(<App/>, document.getElementById('app'));
+
+import About from "../About";
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={About}>
+      <Route path="archives" component={Settings}></Route>
+
+    </Route>
+  </Router>,
+app);
